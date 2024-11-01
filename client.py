23f0335e565c525e60243370d979fd72df031426
maria <mariaeduardascaetano@gmail.com>
2024-11-01 16:51:52 -0300

    FEATURE/message serve > client funcionando

--- client.py
+++ client.py
@@ -1,32 +1,37 @@
 import socket
 import threading 
 
-HOST = '127.0.0.1'
-PORT = 55557
+#HOST = '127.0.0.1'
+#PORT = 55557
 
-alias = input('Choose an alias:')
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
-    c.connect((HOST, PORT))
+alias = input('Choose an alias>>>')
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+client.connect(('127.0.0.1', 55557))
 
 
 def client_receive():
     while True:
          try:
-            data = c.recv(1024).decode('utf-8')
+            data = client.recv(1024).decode('utf-8')
             if data == "alias?":
-                c.send(alias.encode('utf-8'))
+                client.send(alias.encode('utf-8'))
             else: 
                 print(data)
          except:
-            print('ERRO!')
-            c.close()
+            print('ERRO ao receber data!')
+            client.close()
             break
 def client_send():
     while True:
-        data = f'{alias}: {input("")}'
-        c.send(data.encode('utf-8'))
+        try:
+            data = f'{alias}: {input("")}'
+            client.send(data.encode('utf-8'))
+        
+        
+        except:
+            print('ERRO ao enviar dados')
+            client.close()
+            break
 
 receive_thread =  threading.Thread(target=client_receive)
 receive_thread.start()
--- serve.py
+++ serve.py
@@ -11,6 +11,11 @@ reserved for specific services)
 HOST = '127.0.0.1'
 PORT = 55557
 
+serve = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serve.bind((HOST,PORT))
+serve.listen()
+
 #empty list to store connected clients
 clients = []
 aliases = []
@@ -28,66 +33,36 @@ def handleClient(client):
         except:
              index = clients.index(client)
              clients.remove(client)
+            
              client.close()
              alias = aliases[index]
-             broadcast(f'Connection with{alias} was reset')
+             
+             broadcast(f'Connection with{alias} was reset'.encode('utf-8'))
+             
              aliases.remove(alias)
              break
 # main function to connect all clients
 def receive():
-     while True: 
+     while True:
          print('Serve is running and trying connect all clients...')   
-         client, addr = s.accept()
+         client, addr = serve.accept()
          print(f"Connected by {str(addr)} has been established")
+         
          client.send('alias?'.encode('utf-8'))
          alias = client.recv(1024)
+         
          aliases.append(alias)
          clients.append(client)
+         
          print(f'The alias of this client is {alias}'.encode('utf-8'))
-         broadcast(f'{alias.decode("utf-8")} has connected to th3e chat room'.encode('utf-8'))
+         broadcast(f'{alias} has connected to the chat room'.encode('utf-8'))
          client.send('you are now connected'.encode('utf-8'))
+         
          thread = threading.Thread(target=handleClient, args=(client,))
          thread.start()
               
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    # binds this socket to a specific host/port
-    s.bind((HOST, PORT))
-    s.listen()
-    receive()          
+if __name__ == "__main__":  
+    receive()     
             
     
-    
-###############################################################
-    '''HOST = "127.0.0.1"
-
-    PORT = 55557
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        # binds this socket to a specific host/port
-         s.bind((HOST, PORT))
-         s.listen(2)
-    
-         #empty list to store connected clients
-         clients = []
-         aliases = []        
-        
-        
-    while True:
-        conn, addr = s.accept()
-
-        with conn: 
-            print(f"Connected by {addr} has been established1")
-            conn.send("Welcome to the serve!".encode('utf-8'))
-            while True: 
-                try:
-                    data =  conn.recv(1024).decode('utf-8')
-                    if not data: 
-                        break
-                    print(f"Message from client is: {data}")
-                    conn.sendall(data.encode('utf-8'))
-                except ConnectionResetError:
-                    print(f"Connection with{addr} was reset")
-                    break
-    conn.close()'''
-       
     
